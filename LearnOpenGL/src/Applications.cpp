80f782488925e879f502455d188af5579811331e
jifaley <609898697@qq.com>
2024-10-28 00:37:03 +0800

    Lect_23 Render 2 objects with ImGui

--- LearnOpenGL/src/Applications.cpp
+++ LearnOpenGL/src/Applications.cpp
@@ -67,10 +67,10 @@ int main(void)
 
 		float positions[] =
 		{
-			100.0f, 100.0f,  0.0f, 0.0f, //0
-			200.0f, 100.0f,  1.0f, 0.0f,//1
-			200.0f, 200.0f,  1.0f, 1.0f,//2
-			100.0f, 200.0f,  0.0f, 1.0f//3
+			-50.0f, -50.0f,  0.0f, 0.0f, //0
+			 50.0f, -50.0f,  1.0f, 0.0f,//1
+			 50.0f,  50.0f,  1.0f, 1.0f,//2
+			-50.0f,  50.0f,  0.0f, 1.0f//3
 		};
 		//一行四个数，前两个位置坐标，后两个纹理坐标
 
@@ -100,7 +100,7 @@ int main(void)
 
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
 		//投影后得到了更小的logo
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 		//值是负的，因为移动相机实际上相当于反向移动物体。在这里我们只能移动物体
 
 		Shader shader("res/shaders/Basic.shader");
@@ -126,7 +126,8 @@ int main(void)
 		ImGui_ImplGlfwGL3_Init(window, true);
 		ImGui::StyleColorsDark();
 
-		glm::vec3 translation(200, 200, 0);
+		glm::vec3 translationA(200, 200, 0);
+		glm::vec3 translationB(400, 200, 0);
 
 		float r = 0.0f;
 		float increment = 0.05f;
@@ -140,22 +141,24 @@ int main(void)
 
 			ImGui_ImplGlfwGL3_NewFrame(); //在任何imgui调用之前需要newframe
 
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-			glm::mat4 mvp = proj * view * model;
-
 			
 
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4f("u_MVP", mvp);
+				renderer.Draw(va, ib, shader); //va,ib在Draw()函数中自带了Bind()，因此不在这里再显式绑定了
+			}
 
-			shader.Bind();
-			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-			shader.SetUniformMat4f("u_MVP", mvp);
-
-			va.Bind();
-			ib.Bind();
-
-			renderer.Draw(va, ib, shader);
-
-
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4f("u_MVP", mvp);
+				renderer.Draw(va, ib, shader);
+			}
+			
 			//注意！这些int都需要是无符号类型！
 			if (r > 1.0f)
 				increment = -0.05f;
@@ -165,7 +168,8 @@ int main(void)
 
 
 			{
-				ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f); 
+				ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f); //这里的float3提供第一个float地址后会自动绑定后面的两个float
+				ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
 				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 			}
 
